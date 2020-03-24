a618ccad989d1e2c94b3a8f93cfc7d947ff0fbf9
mughees-asif <mughees460@gmail.com>
2020-03-24 13:22:55 +0000

    adjusting image about us

--- src/logged_out/components/blog/Blog.js
+++ src/logged_out/components/blog/Blog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useLayoutEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import topAboutUsImage from "../../dummy_data/images/topAboutUsImage.png";
 import bottomHeaderImage from "../../dummy_data/images/bottomHeaderImage.png";
@@ -9,62 +9,63 @@ import PaymentIcon from '@material-ui/icons/Payment';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 
 const useStyles = makeStyles({
-
 });
 
 export default function MediaCard() {
-  const classes = useStyles();
 
+  const classes = useStyles();
   return (
-   <div style={{ backgroundColor: "black" }}>
-    <div className="container" >
-      <div className="container" display ="flex" flexDirection="row" style={{ paddingTop: "5rem", position: "relative" }}>
+    <Fragment>
+     <div style={{ backgroundColor: "black" }}>
+      <div className="container" >
+        <div className="container-icons" display ="flex" flexDirection="row" style={{ paddingTop: "5rem" }}>
+            <img
+              src={topAboutUsImage}
+              className={classes.image}
+              style={{ width: '50rem', maxWidth: 'fill-available', maxHeight: "fill-available", display: "flex", flexDirection: "column" }}
+              alt="topAboutUsImage example"
+            />
+        </div>
+      </div>
+      <div className="container">
+        <div className="container-image" style= {{ width:"100%" }}>
+            <Card variant="outlined" style={{ backgroundColor: "black" }} >
+              <Typography variant="h5" component="p" style={{ color: "white" }}>
+                <p>Tutoring software that will seamlessly transform remote education by integrating 
+                  online classrooms, Machine Learning, Data Analytics, Automated booking 
+                  systems, and real-time collaboration, where, keeping track of everything is happening under your virtual roof!</p>
+                  <p>The customisable CRM dashboard helps to manage daily operations such as contaacting tutors, 
+                  managing students, monitoring sessions, handling reviews, transaction histories, and invoices to give you 
+                  full control.</p>
+                <div className="container-icons" >
+                    <PaymentIcon style={{ fontSize: 100, color: "blue" }} />
+                      <ol>Powered by Stripe payment</ol>
+                </div>
+                 <div className="container-icons" > 
+                    <NotificationsActiveIcon  style={{ fontSize: 100, color: "white" }} />         
+                      <ol>Automatic text-notification system</ol>
+                </div>
+                <div className="container-icons" >
+                  <ScheduleIcon style={{ fontSize: 100, color: "green" }} />
+                    <ol>Machine Learning algorithms suggest improvements</ol>
+                </div>
+                <div className="container-icons" >
+                  <AssessmentIcon style={{ fontSize: 100, color: "red" }} />
+                    <ol>Advanced analytics allows for real-time tracking</ol>
+                </div>
+              </Typography>
+            </Card>
+         </div> 
+      </div>    
+        <div className="container-icons">
           <img
-            src={topAboutUsImage}
-            className={classes.image}
-            style={{ width: '50rem' }}
-            alt="topAboutUsImage example"
+          src={bottomHeaderImage}
+          className={classes.image}
+          alt="bottomHeaderImage example"
+          style={{ width: '15rem' }}
           />
+        </div>
       </div>
-    </div>
-    <div className="container">
-      <div className="container-image" style= {{ width:"100%" }}>
-          <Card variant="outlined" style={{ backgroundColor: "black" }} >
-            <Typography variant="h5" component="p" style={{ color: "white" }}>
-              <p>Tutoring software that will seamlessly transform remote education by integrating 
-                online classrooms, Machine Learning, Data Analytics, Automated booking 
-                systems, and real-time collaboration, where, keeping track of everything is happening under your virtual roof!</p>
-                <p>As a tutoring company you can use our simple CRM dashboard to manage daily operations such as contaacting tutors, 
-                managing students, monitoring sessions, handling reviews, transaction histories, and invoices to give you 
-                full control.</p>
-              <div className="container-icons" >
-                  <PaymentIcon style={{ fontSize: 100, color: "blue" }} />
-                    <ol>Powered by Stripe payment</ol>
-              </div>
-               <div className="container-icons" > 
-                  <NotificationsActiveIcon  style={{ fontSize: 100, color: "white" }} />         
-                    <ol>Automatic text-notification system</ol>
-              </div>
-              <div className="container-icons" >
-                <ScheduleIcon style={{ fontSize: 100, color: "green" }} />
-                  <ol>Machine Learning algorithms suggest improvements</ol>
-              </div>
-              <div className="container-icons" >
-                <AssessmentIcon style={{ fontSize: 100, color: "red" }} />
-                  <ol>Advanced analytics allows for real-time tracking</ol>
-              </div>
-            </Typography>
-          </Card>
-       </div> 
-    </div>    
-      <div className="container-icons">
-        <img
-        src={bottomHeaderImage}
-        className={classes.image}
-        alt="bottomHeaderImage example"
-        style={{ width: '15rem' }}
-        />
-      </div>
-    </div>
+    </Fragment>
   );
 }
